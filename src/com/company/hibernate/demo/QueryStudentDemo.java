2e351fcd5a44fb954f466c20bfbdb0e866ef314b
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-24 14:58:05 +0100

    Query the database for all students in the database

--- src/com/company/hibernate/demo/QueryStudentDemo.java
+++ src/com/company/hibernate/demo/QueryStudentDemo.java
@@ -5,28 +5,30 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class QueryStudentDemo {
 
     public static void main(String[] args) {
         //Create session factory
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
 
         //Create session
         Session session = factory.getCurrentSession();
 
         try {
             //Use the session object to save Java Object
-
-            //Create  a student object
-            System.out.println("Creating new student object... ");
-            Student tempStudent = new Student("Paul", "Wall", "paul@luv2code.com");
-
             //Start a transaction
             session.beginTransaction();
 
-            //Save the student object
-            System.out.println("Saving the student");
-            session.save(tempStudent);
+            //Query Students
+           List<Student> theStudents = session.createQuery("from Student").list();
+           for (Student tempStudent:theStudents) {
+               System.out.println(tempStudent);
+           }
 
             //Commit transaction
             session.getTransaction().commit();
